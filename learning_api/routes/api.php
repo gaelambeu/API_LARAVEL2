69b5a4b7709d0a67424ebffa42c9efad81195c7f
Gael16062003 <ambeugael128@gmail.com>
2023-05-27 23:53:49 +0200

    premier point de terminaison

--- learning_api/routes/api.php
+++ learning_api/routes/api.php
@@ -14,6 +14,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Crée un lien qui permettra aux clients : React, Vue, Angular, Node, Js Native
+Route::get('test', function () {
+    return 'test';
+});
+
+
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
