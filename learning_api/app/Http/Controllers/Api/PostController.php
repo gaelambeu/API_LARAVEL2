8ad61059f6a5fbfbb66d468f89dec0fb67539385
Gael16062003 <ambeugael128@gmail.com>
2023-05-28 02:45:11 +0200

    comprendre le Try & le Catch pour gérer les imprévus

--- learning_api/app/Http/Controllers/Api/PostController.php
+++ learning_api/app/Http/Controllers/Api/PostController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\CreatePostRequest;
 use App\Models\Post;
+use Exception;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
@@ -15,17 +16,22 @@ class PostController extends Controller
 
    public function store(CreatePostRequest $request){
 
-        // dd($request);
-        $post = new Post();
-        $post->titre = $request->titre;
-        $post->description = $request->description;
-        $post->save();
+        try {
+                // dd($request);
+            $post = new Post();
+            $post->titre = $request->titre;
+            $post->description = $request->description;
+            $post->save();
 
-        return response() -> json([
-            'status_code' => 200,
-            'status_message' => 'Le post a été ajouté',
-            'date' => $post
+            return response() -> json([
+                'status_code' => 200,
+                'status_message' => 'Le post a été ajouté',
+                'date' => $post
         ]);
+        
+        } catch (Exception $e) {
+            return response() -> json($e);
+        }
    }
 }
 
