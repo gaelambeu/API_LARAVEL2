7fc13efea38cd60d376158b34f7e32a529543ae2
Gael16062003 <ambeugael128@gmail.com>
2023-05-29 19:54:08 +0200

    Retourner des données après création de compte

--- learning_api/app/Http/Controllers/Api/UserController.php
+++ learning_api/app/Http/Controllers/Api/UserController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\RegisterUser;
 use App\Models\User;
+use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 
@@ -12,13 +13,25 @@ class UserController extends Controller
 {
     public function register(RegisterUser $request)
     {
-        $user = new User();
-        $user->name = $request->name;
-        $user->email = $request->email;
-        $user->password = Hash::make($request->password,[
-            'rounds' => 12
-        ]);
 
-        $user->save();
+        try{
+                $user = new User();
+                $user->name = $request->name;
+                $user->email = $request->email;
+                $user->password = Hash::make($request->password,[
+                    'rounds' => 12
+                ]);
+
+                $user->save();
+
+                return response()->json([
+                'status_code' => 200,
+                'status_message' => 'Utilisateurs enregistré',
+                'user' => $user
+            ]);
+
+        } catch(Exception $e){
+                return response()->json($e);
+        }
     }
 }
